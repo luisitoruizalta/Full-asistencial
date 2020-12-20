698821dde7a9f671529ade5aa07e85b36239566b
luisitoruizalta <76391607+luisitoruizalta@users.noreply.github.com>
2020-12-20 18:02:05 +0100

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,16 +1,9 @@
-<?php
-
-if (isset($_POST['enviar'])) ¨{
-	if (!empty($_POST['Unitat']) && !empty($_POST['Tecnics'] !empty($_POST['Num. telefon'])) {
-		$Unitat $_POST['Unitat'];
-		$Tecnics $_POST['Tecnics'];
-		$Telefon $_POST['Num. telefon'];
-		$header = "From: noreply@example.com" . "\r\n";
-		$header.= "Reply-To: noreply@example.com" . "\r\n";
-		$header.= "X-Mailer: PHP/". phpversion();
-		$mail = @luisitoruizalta  ($Unitat,$Tecnics,$Telefon,$header);
-			echo "<h4>¡Mail enviat correctament!</h4>";
-		}
-	}
-}
+<?php 
+	$destino= "luisitoruizalta@gmail.com";
+	$nombre= $_POST["Unitat"];
+	$nombre= $_POST["Tecnics"];
+	$nombre= $_POST["Telefon"];
+	$contenido= "Unitat: " . $Unitat . "\nTecnics: " . $Tecnics . "\nTelefon: " . $Telefon;
+	mail($destino, "Full asistencial", $contenido);
+	header("location:asistencial.html");
 ?>
