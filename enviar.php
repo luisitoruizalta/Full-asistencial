4efeb3268a7b7cbc4faefe75ddc2fd3476b7461a
luisitoruizalta <76391607+luisitoruizalta@users.noreply.github.com>
2020-12-22 14:41:18 +0100

    Delete enviar.php

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$destino= "luisitoruizalta@gmail.com";
-	$nombre= $_POST["Unitat"];
-	$nombre= $_POST["Tecnics"];
-	$nombre= $_POST["Telefon"];
-	$contenido= "Unitat: " . $Unitat . "\nTecnics: " . $Tecnics . "\nTelefon: " . $Telefon;
-	mail($destino, "Full asistencial", $contenido);
-	header("location:asistencial.html");
-?>
